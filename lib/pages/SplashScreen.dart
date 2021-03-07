49491f6d25311e3bd983270ea0a52babb8419a30
Ryan Cooke <ryanjcooke@hotmail.com>
2021-03-07 19:38:39 +0700

    after getting the first inital splash screen done

--- lib/pages/SplashScreen.dart
+++ lib/pages/SplashScreen.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class SplashScreen extends StatefulWidget {
-  final Color backgroundColor = Colors.red;
   final TextStyle styleTextUnderTheLoader = TextStyle(
       fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.black);
 
@@ -36,27 +35,67 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     double statusBarHeight = MediaQuery.of(context).padding.top;
 
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     SystemChrome.setSystemUIOverlayStyle(
         SystemUiOverlayStyle(statusBarColor: Colors.blue));
 
+    double bgOpacity = 0.45;
+
     return Scaffold(
-        body: Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-                color: Colors.blue,
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height - statusBarHeight,
-                child: Stack(children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
+      body: Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          color: Colors.blue,
+          width: double.infinity,
+          height: screenHeight - statusBarHeight,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                left: -(screenWidth * 1.05),
+                top: -(screenWidth),
+                child: Transform.rotate(
+                  angle: 158.9,
+                  child: Opacity(
+                    opacity: bgOpacity,
+                    child: Image(
+                      image: AssetImage('lib/assets/earth2.png'),
+                      width: 1000,
+                    ),
                   ),
-                  Container(
-                    width: 90,
-                    height: 90,
-                    color: Colors.green,
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  Expanded(
+                    child: Center(
+                      child: Image(
+                        image: AssetImage('lib/assets/my-e2.png'),
+                        width: 100,
+                      ),
+                    ),
                   ),
-                ]))));
+                ],
+              ),
+              Positioned(
+                left: -(screenWidth / 5),
+                top: (screenHeight * 0.25),
+                child: Transform.rotate(
+                  angle: 25.2,
+                  child: Opacity(
+                    opacity: bgOpacity,
+                    child: Image(
+                      image: AssetImage('lib/assets/earth2.png'),
+                      width: 1000,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
